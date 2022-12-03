b4334b6cf0d933b39006289ce83800bf117e30d0
Wesakania <wesakaniadavis@gmail.com>
2022-12-03 17:22:48 +0300

    today'sExercise

---
+++ adventure.js
@@ -0,0 +1,26 @@
+/*let salutation;
+function greetings(){
+ salutation = prompt("Good morning girl");
+ document.getElementById("try").innerHTML = salutation;
+}
+greetings();
+
+function jewel(){
+  let salutation = prompt("How old are you?")
+  let age = salutation
+  if(age <= 18){
+    document.getElementById("try").innerHTML = "You are too young!";
+  }else if( age <= 25){
+    document.getElementById("try").innerHTML = "Do you have a driving licence?"
+}else{
+  document.getElementById("try").innerHTML = "Go home and study for exams!"
+}
+  }
+  
+jewel();
+*/
+function myStatus(){
+  let salutation =  document.getElementById("cry").value;
+  document.getElementById("demo").innerHTML = salutation;
+}
+
